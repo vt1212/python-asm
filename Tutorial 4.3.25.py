63a5acc0266e0a2c70188be6f2e0b594ff9b02d3
vt1212 <79259391+vt1212@users.noreply.github.com>
2025-03-07 18:37:53 +0800

    Initial commit

---
+++ Tutorial 4.3.25.py
@@ -0,0 +1,44 @@
+from sympy.codegen.fnodes import intent_out
+from sympy.physics.units import kilometers
+
+x,y=8,5
+#do division
+#x/y
+x,y=8,5
+print(x//y)
+
+#Question 2
+#kilometers=int(input('Enter the distance in kilometers:'))
+#miles=kilometers/0.6214
+#print("The distance in miles is:",miles)
+
+#Question 3
+#greeting='Hello' *5
+#print(greeting)
+
+#Question 5
+#b=float(input('Enter the base:'))
+#h=float(input('Enter the height:'))
+#area=b*h/2
+#print('Area',area)
+"""
+name=input('Enter Employee Name:')
+salary=input("Enter salary:") #small capital of salary
+company=input('Enter Company Name:')
+print('\n') # a new empty line
+print('Printing Employee Details')
+print('Name Salary Company')
+#Another version: print ('Name Salary Company')("Name","Salary","Company") to space each word
+print(name,salary,company)
+"""
+'''
+#Question 4
+P=float(input('Principle Amount:'))
+T=float(input("Time:"))
+R=float(input("Rate:"))
+print('\n')
+formula=((P*T*R)/100)
+print('Answer:',formula)
+'''
+
+
